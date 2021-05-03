6bf2e25806c396f64df2c3f9931fa006db002b97
xyqfer <xyqfer@gmail.com>
2021-05-03 19:14:05 +0800

    update

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+app.enable('trust proxy');
+
+app.use(cors({
+  origin: '*',
+}));
+
+app.use(
+  '/',
+  createProxyMiddleware({ 
+    target: 'https://api.twitter.com', 
+    changeOrigin: true,
+    onProxyReq(proxyReq, req, res) {
+      // proxyReq.setHeader('origin', 'https://mobile.twitter.com');
+      proxyReq.setHeader('referer', 'https://mobile.twitter.com/');
+    },
+  }),
+);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
